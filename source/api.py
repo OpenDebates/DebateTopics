407848e7e43b585f5e9ef17d6333f1f0c7579df1
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-15 18:11:11 -0500

    Update api.py

--- source/api.py
+++ source/api.py
@@ -10,6 +10,25 @@ class TokenRequiredError(Exception):
   def __repr__(self) -> str:
     return "A token is required when making API calls."
 
+class CategoryRequiredError(Exception):
+  def __repr__(self) -> str:
+    return "Category is a required argument that is missing."
+
+class InvalidCategoryError(Exception):
+  def __repr__(self) -> str:
+    return "The category you provided is not a valid category. Please request '/categories' to see a list of categories"
+ 
+class TopicRequiredError(Exception):
+  def __repr__(self) -> str:
+    return "Topic is a required argument that is missing.
+
+class TopicExistsError(Exception):
+  def __init__(self, topic: str) -> None:
+    self.topic=topic
+    
+  def __repr__(self) -> str:
+    return "The topic {} already exists.".format(self.topic)
+  
 #home
 @app.route(
   '/',
@@ -19,6 +38,8 @@ class TokenRequiredError(Exception):
   ]
 )
 def _home() - > str:
+  if utils.check_token(request.json) == False:
+    raise TokenRequiredError()
   return "<h1>OpenDebates Debate Topics API</h1> <p>version - 1.0-alpha</p1>", 200
 
 #version route
@@ -30,7 +51,7 @@ def _home() - > str:
   ]
 )
 def _home() -> str:
-  if utils.check_token(request.args) == False:
+  if utils.check_token(request.json) == False:
     raise TokenRequiredError()
   return "<h1>OpenDebates Debate Topics API</h1> <p>version - 1.0-alpha</p1>", 200
 
@@ -40,20 +61,116 @@ def _home() -> str:
   methods = [
     'GET',
     'POST'
-  ])
+  ]
+)
 def _get_topic() -> dict:
-  if utils.check_token(request.args) == False:
+  if utils.check_token(request.json) == False:
     raise TokenRequiredError()
-  if "amount" not in request.args:
+  if "amount" not in request.json:
     return jsonify([{
       "topic": debate_topics.get_topic()
     }]), 200
   else:
     topics = []
-    for i in range(request.args):
+    for i in range(request.json["amount"]):
       topics.append(debate_topics.get_topic())
     return jsonify([{
       "topics": topics
     }]), 200
+  
+  
+@app.route(
+  '/get_topic/category',
+  methods =[
+    'GET',
+    'POST'
+  ]
+)
+def _get_topic_category()-> dict:
+  if utils.check_token(request.json) == False:
+    raise TokenRequiredError()
+  if "category" not in request.json:
+    raise CategoryRequiredError()
+  if request.json["category"] not in debate_topics.topic_categories:
+    raise InvalidCategoryError()
+    
+    
+  if "amount" not in request.json:
+    return jsonify([{
+      "topic": debate_topics.get_selected_topic(
+        request.json["category"]
+      )
+    }]), 200
+  
+  else:
+    topics = []
+    for i in range(request.json["amount"]):
+      topics.append(debate_topics.get_selected_topic(
+        request.json["category"]
+      ))
+    return jsonify([{
+      "topics": topics
+    }]), 200
+  
+@app.route(
+  '/get_topic/categories',
+  methods=[
+    'GET',
+    'POST'
+  ]
+)
+def _get_topic_categories() -> dict:
+  if utils.check_token(request.json) == False:
+    raise TokenRequiredError()
+  if "categories" not in request.json:
+    raise CategoryRequiredError()
+  if len(request.json['categories']) == 0 or len(request.json['categories']) == 1:
+    raise CategoryRequiredError()
+  for category in request.json['categories']:
+    if category not in debate_topics.topic_categories:
+      raise InvalidCategoryError()
+    
+    
+  if "amount" not in request.json:
+    return jsonify([{
+      "topic": debate_topics.get_topic_from_selections(
+        request.json["categories"]
+      )
+    }]), 200
+  
+  else:
+    topics = []
+    for i in range(request.json["amount"]):
+      topics.append(debate_topics.get_topic_from_selections(
+        request.json["categories"]
+      ))
+    return jsonify([{
+      "topics": topics
+    }]), 200
+  
+@app.route(
+  '/add_topic',
+  methods = [
+    'GET',
+    'POST'
+  ]
+)
+def _add_topic() -> dict:
+  if utils.check_token(request.json) == False:
+    raise TokenRequiredError()
+  if "category" not in request.json:
+    raise CategoryRequiredError()
+  if request.json["category"] not in debate_topics.topic_categories:
+    raise InvalidCategoryError()
+  if "topic" not in request.json:
+    raise TopicRequiredError()
+    
+  if debate_topics.add_topic(request.json["topic"], request.json["category"]):
+    return jsonify([{
+      "topic": request.json["topic"]
+    }]), 200
+  
+  else:
+    raise TopicExistsError(request.json['topic'])
   
   app.run("0.0.0.0")
