8e4fe753de3f8728d4660572a9db42ef1cc004ef
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-15 16:49:14 -0500

    Create utils.py

---
+++ source/utils.py
@@ -0,0 +1,45 @@
+import typing
+import secrets
+import json
+from random import *
+
+def validate_token(
+  args: typing.List(
+    typing.Union(
+      str,
+      int
+    )
+  ) = []
+) -> bool:
+  
+  with open("tokens.json", "r+") as f:
+    tokens = json.load(f)
+  
+  if len(args) == 0:
+    return False
+  else if "token" not in args:
+    return False
+  else if args["token"] not in tokens:
+    return False
+  
+  else:
+    return True
+  
+
+  
+def create_token() -> str:
+  with open("tokens.json", "r+") as f:
+    tokens=json.load(f)
+  
+  while True:
+    new_token = secrets.urlsafe(randint(32, 40))
+    if new_token in tokens:
+      continue
+    
+    else:
+      token=new_token
+      break
+  tokens[token] = "1"
+  with open("tokens.json", "w+") as f:
+    json.dump(tokens, f)
+  return token
