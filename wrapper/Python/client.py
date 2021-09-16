fecc1ab8948dc64c8fdbe49861aad033fe5a339f
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-15 23:10:59 -0500

    Create client.py

---
+++ wrapper/Python/client.py
@@ -0,0 +1,117 @@
+import typing
+import asyncio
+from Classes import *
+
+class Topic:
+  def __init__(
+    self,
+    content: str
+  ) -> None:
+    self.content=content
+    
+  def __repr__(
+    self
+  ) -> str:
+    return self.content
+  
+  def __str__(
+    self
+  ) -> str:
+    return self.content
+
+class Client:
+  def __init__(
+    self,
+    token: str
+  ) -> None:
+    self.token=token
+    
+  async def get_topic(
+    amount: int=1
+  ) -> typing.Union(Topic, typing.List(Topic)):
+    req = TopicRequest(
+      amount=amount,
+      token=self.token
+    )
+    resp = asyncio.get_event_loop().run_until_complete(req.exec())
+    
+    if "topics" in resp.json():
+      topics = []
+      index = 0
+      for topic in resp.json()["topics"]:
+        topics.append(
+          Topic(
+            resp.json()["topics"][index]
+          )
+          index+=1
+      return topics
+        
+    return Topic(
+      resp.json()["topic"]
+    )
+  
+  async get_topic_by_category(
+    category: str,
+    amount: int=1
+  ) -> typing.Union(Topic, typing.List(Topic)):
+    req = TopicRequest(
+      categories=category,
+      amount=amount,
+      token=self.token
+    )
+    resp = asyncio.get_event_loop().run_until_complete(req.exec())
+    
+    if "topics" in resp.json():
+      topics = []
+      index = 0
+      for topic in resp.json()["topics"]:
+        topics.append(
+          Topic(
+            resp.json()["topics"][index]
+          )
+          index+=1
+      return topics
+        
+    return Topic(
+      resp.json()["topic"]
+    )
+    
+  async get_topic_by_categories(
+    categories: str,
+    amount: int=1
+  ) -> typing.Union(Topic, typing.List(Topic)):
+    req = TopicRequest(
+      categories=categories,
+      amount=amount,
+      token=self.token
+    )
+    resp = asyncio.get_event_loop().run_until_complete(req.exec())   
+    if "topics" in resp.json():
+      topics = []
+      index = 0
+      for topic in resp.json()["topics"]:
+        topics.append(
+          Topic(
+            resp.json()["topics"][index]
+          )
+          index+=1
+      return topics
+        
+    return Topic(
+      resp.json()["topic"]
+    )
+          
+  async def add_topic(
+    topic: str,
+    category: str
+  ) -> bool:
+    req = AddTopicRequest(
+      topic=topic,
+      category=category,
+      token=self.token
+    )
+    try:
+      asyncio.get_event_loop().run_until_complete(req.exec()) 
+      return True
+    except:
+      return False
